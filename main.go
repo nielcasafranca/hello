465f81d49f90ae608c49e829ee7278097e63aea4
Niel Casafranca <nielcasafranca@gmail.com>
2022-01-05 11:11:46 -0500

    Final commit

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/nielcasafranca/hello/greet"
+	"rsc.io/quote/v3"
+)
 
 func main() {
-	fmt.Println("Hello")
-
-	greet.English()
+	fmt.Println(greet.Spanish())
+	fmt.Println(quote.HelloV3())
+	fmt.Println(quote.Concurrency())
 }
